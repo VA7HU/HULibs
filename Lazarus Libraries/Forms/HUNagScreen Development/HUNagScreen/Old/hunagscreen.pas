bba9492ebd3f67e29595d2907757cfb4ea1cb6b7
Hew Lines <va7hu@shaw.ca>
2018-09-29 19:24:52 -0700

    Work on HUNagScreen

---
+++ Lazarus Libraries/Forms/HUNagScreen Development/HUNagScreen/Old/hunagscreen.pas
@@ -0,0 +1,26 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmHUNagScreen = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmHUNagScreen: TfrmHUNagScreen;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ Lazarus Libraries/Forms/HUNagScreen Development/HUNagScreen/backup/hunagscreen.pas
@@ -0,0 +1,26 @@
+unit HUNagScreen;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmHUNagScreen = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmHUNagScreen: TfrmHUNagScreen;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
