bdcc0bb72bc32e8e3697cce482e1b8daba27b8ad
Hew Lines <va7hu@shaw.ca>
2018-10-07 09:39:10 -0700

    Work on HURegister

---
+++ Lazarus Libraries/Forms/HURegister Development/HURegister2/backup/main.pas
@@ -0,0 +1,42 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+//========================================================================================
+//
+// Unit :
+//
+// Description :
+//
+// Called By :
+//
+// Calls :
+//
+// Ver. : 1.00
+//
+// Date :
+//
+//========================================================================================
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ Lazarus Libraries/Forms/HURegister Development/HURegisterTest/backup/main.pas
@@ -0,0 +1,39 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Buttons{,
+  HURegister};
+
+type
+
+  { TfrmMain }
+
+  TfrmMain = class(TForm)
+    bbtTest1: TBitBtn;
+    procedure bbtTest1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.lfm}
+
+{ TfrmMain }
+
+procedure TfrmMain.bbtTest1Click(Sender: TObject);
+begin
+  dlgHURegister.ShowModal;
+end;
+
+end.
+
